95db9d940901d65031248c390017c84e7ae337a4
PINOT <pinot.kim@kakao.com>
2021-12-21 01:07:10 +0900

    Add `justify` parameter at `AlignCenter` component
    - 좌우 정렬에 대응하는 파라미터 추가

--- components/AlignCenter.tsx
+++ components/AlignCenter.tsx
@@ -1,8 +1,13 @@
 import styled from "styled-components";
 import Flex from "./Flex";
 
-const AlignCenter = styled(Flex)`
+interface Props {
+	justify?: boolean;
+}
+
+const AlignCenter = styled(Flex)<Props>`
 	align-items: center;
+	justify-content: ${({ justify }) => (justify ? "center" : null)};
 	height: 100%;
 `;
 
