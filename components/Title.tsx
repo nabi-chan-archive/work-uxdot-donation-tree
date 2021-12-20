297cd1fb5250fdca96597d0ea9216aabef4ad23f
PINOT <pinot.kim@kakao.com>
2021-12-20 20:12:05 +0900

    Add `Title` component

---
+++ components/Title.tsx
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+const Title = styled.h1`
+	font-weight: bold;
+	font-size: 36px;
+	line-height: 43px;
+	white-space: pre-line;
+`;
+
+export default Title;
