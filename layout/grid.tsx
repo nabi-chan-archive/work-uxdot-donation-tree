f8c5bb0f95e93d9a73e7eaea1c757e7fe80fae81
PINOT <pinot.kim@kakao.com>
2021-12-20 21:21:22 +0900

    Add `Grid` layout

---
+++ layout/grid.tsx
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+const Grid = styled.div`
+	display: grid;
+	height: 100%;
+	width: 100vw;
+	grid-template-columns: 580fr 786fr;
+`;
+
+export default Grid;
