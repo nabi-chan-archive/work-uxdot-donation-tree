5579f9acefe0410d3df9937987923cc2c85c3438
PINOT <pinot.kim@kakao.com>
2021-12-22 03:08:32 +0900

    Remove rewrite to 404

--- pages/create/_middleware.ts
+++ pages/create/_middleware.ts
@@ -1,10 +1,6 @@
 import { NextRequest, NextResponse } from "next/server";
 
 function _middleware(req: NextRequest) {
-	if (req.url !== "/create") {
-		return NextResponse.rewrite("/404");
-	}
-
 	if (!req.cookies.accessToken) return NextResponse.rewrite("/create");
 
 	NextResponse.next();
