3bb17465df433ad58ccfb407362f0d80beafef53
PINOT <pinot.kim@kakao.com>
2021-12-22 08:58:29 +0900

    Add relative position on `Absolute` component

--- components/Absolute.tsx
+++ components/Absolute.tsx
@@ -9,10 +9,11 @@ interface Props {
 
 const Absolute = styled.div<Props>`
 	position: absolute;
-	top: ${({ top }) => top};
-	left: ${({ left }) => left};
-	right: ${({ right }) => right};
-	bottom: ${({ bottom }) => bottom};
+	top: ${({ top }) => (typeof top === "number" ? `${top}px` : top)};
+	left: ${({ left }) => (typeof left === "number" ? `${left}px` : left)};
+	right: ${({ right }) => (typeof right === "number" ? `${right}px` : right)};
+	bottom: ${({ bottom }) =>
+		typeof bottom === "number" ? `${bottom}px` : bottom};
 `;
 
 export default Absolute;
