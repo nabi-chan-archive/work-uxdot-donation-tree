48c84f9986cc4d1518ec4624112a49c29002c6f2
PINOT <pinot.kim@kakao.com>
2021-12-22 08:44:29 +0900

    Add metatag for SEO

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,5 @@
 import type { AppProps } from "next/app";
+import Head from "next/head";
 import GlobalStyle from "../styles/GlobalStyle";
 import { Reset } from "styled-reset";
 import { ThemeProvider } from "styled-components";
@@ -10,6 +11,43 @@ import ToastContextProvider from "../context/toast.context";
 function MyApp({ Component, pageProps }: AppProps) {
 	return (
 		<>
+			<Head>
+				<meta name="theme-color" content="#8c391b" />
+				<title>메타버스 기부 프로젝트 : 스노우타운</title>
+				<meta
+					name="description"
+					content={
+						"메타버스 세상속 스노우타운에서 곤경에 처한 사람들을 도와주세요! " +
+						"미션들을 수행하고 트리를 완성하면 천원이 자동으로 기부됩니다!"
+					}
+				/>
+
+				<meta name="twitter:card" content="summary_large_image" />
+				<meta name="twitter:site" content="@pinot_kim" />
+				<meta
+					name="twitter:image"
+					content="https://donation-tree.io/opengraph_image.png"
+				/>
+
+				<meta property="og:type" content="website" />
+				<meta property="og:locale" content="ko_KR" />
+				<meta
+					property="og:title"
+					content="메타버스 기부 프로젝트 : 스노우타운"
+				/>
+				<meta
+					property="og:description"
+					content={
+						"메타버스 세상속 스노우타운에서 곤경에 처한 사람들을 도와주세요! " +
+						"미션들을 수행하고 트리를 완성하면 천원이 자동으로 기부됩니다!"
+					}
+				/>
+				<meta property="og:url" content="https://donation-tree.io" />
+				<meta
+					property="og:image"
+					content="https://donation-tree.io/opengraph_image.png"
+				/>
+			</Head>
 			<Reset />
 			<ThemeProvider theme={Light}>
 				<GlobalStyle />
